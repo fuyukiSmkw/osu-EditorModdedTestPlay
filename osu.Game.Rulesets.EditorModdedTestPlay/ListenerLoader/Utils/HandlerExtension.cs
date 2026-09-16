463075ce4a86c3427f22af8746c814f7375cbbf7
fuyukiS <fuyukiS@outlook.jp>
2026-09-17 01:21:18 +0800

    Replace PlaybackSpeedControl with FormSliderBar for a wider range (max 3.0) and better precision (0.01)

--- osu.Game.Rulesets.EditorModdedTestPlay/ListenerLoader/Utils/HandlerExtension.cs
+++ osu.Game.Rulesets.EditorModdedTestPlay/ListenerLoader/Utils/HandlerExtension.cs
@@ -12,7 +12,7 @@ namespace osu.Game.Rulesets.EditorModdedTestPlay.ListenerLoader.Utils;
 
 public static class HandlerExtension
 {
-    public const BindingFlags INSTANCE_FLAG = BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.GetProperty | BindingFlags.GetField;
+    public const BindingFlags INSTANCE_FLAG = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.GetProperty | BindingFlags.GetField;
 
     private static FieldInfo? findFieldInstanceInBaseType(Type baseType, Type type)
     {
@@ -38,7 +38,7 @@ public static class HandlerExtension
 
     private static FieldInfo? findFieldInstanceByName(Type baseType, string name)
     {
-        var field = baseType.GetField(name); // INSTANCE_FLAG
+        var field = baseType.GetField(name, INSTANCE_FLAG); // INSTANCE_FLAG
 
         if (field == null && baseType.BaseType != null)
             field = findFieldInstanceByName(baseType.BaseType, name);
@@ -59,7 +59,7 @@ public static class HandlerExtension
 
     private static MethodInfo? findMethodByName(Type baseType, string name, Type returnType)
     {
-        var method = baseType.GetMethod(name); // INSTANCE_FLAG
+        var method = baseType.GetMethod(name, INSTANCE_FLAG);
         if (method == null && baseType.BaseType != null)
             method = findMethodByName(baseType.BaseType, name, returnType);
         return method?.IsGenericMethodDefinition ?? false ? method?.MakeGenericMethod(returnType) : method;
@@ -68,5 +68,54 @@ public static class HandlerExtension
     public static Func<object?[], object?> FindMethod(this object obj, string name, Type returnType)
     {
         return parameters => findMethodByName(obj.GetType(), name, returnType)?.Invoke(obj, parameters);
+    }
+
+    private static Type? findTypeByName(Type baseType, string typeName)
+    {
+        if (baseType.Name == typeName || baseType.Name.Split('`')[0] == typeName)
+            return baseType;
+
+        var nestedTypes = baseType.GetNestedTypes(INSTANCE_FLAG);
+        foreach (var nested in nestedTypes)
+        {
+            var ret = findTypeByName(nested, typeName);
+            if (ret is not null)
+                return ret;
+        }
+        return null;
+    }
+
+    public static Type? FindType(this object obj, string typeName)
+    {
+        return findTypeByName(obj.GetType(), typeName);
+    }
+
+    public static Type? FindType(this Type baseType, string typeName)
+    {
+        return findTypeByName(baseType, typeName);
+    }
+
+    public const BindingFlags PROPERTY_FLAG = BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static | BindingFlags.GetProperty | BindingFlags.DeclaredOnly;
+
+    private static PropertyInfo? findPropertyByName(Type? type, string name)
+    {
+        var prop = type?.GetProperty(name, PROPERTY_FLAG);
+        if (prop != null)
+            return prop;
+        return findPropertyByName(type?.BaseType, name);
+    }
+
+    public static object? GetPropertyValue(this object obj, string propertyName)
+    {
+        var prop = findPropertyByName(obj.GetType(), propertyName) ?? throw new MissingMemberException(propertyName);
+        var getter = prop.GetGetMethod(nonPublic: true) ?? throw new InvalidOperationException($"{propertyName} has no getter");
+        return getter.Invoke(obj, []);
+    }
+
+    public static void SetPropertyValue(this object obj, string propertyName, object? value)
+    {
+        var prop = findPropertyByName(obj.GetType(), propertyName) ?? throw new MissingMemberException(propertyName);
+        var setter = prop.GetSetMethod(nonPublic: true) ?? throw new InvalidOperationException($"{propertyName} has no setter");
+        setter.Invoke(obj, [value]);
     }
 }
--- osu.Game.Rulesets.EditorModdedTestPlay/Screens.Edit/ModdedEditor.cs
+++ osu.Game.Rulesets.EditorModdedTestPlay/Screens.Edit/ModdedEditor.cs
@@ -1,12 +1,17 @@
 using System;
 using System.Linq;
 using osu.Framework.Allocation;
+using osu.Framework.Bindables;
+using osu.Framework.Graphics;
 using osu.Framework.Graphics.Containers;
 using osu.Framework.Screens;
 using osu.Framework.Testing;
 using osu.Game.Graphics.UserInterface;
+using osu.Game.Graphics.UserInterfaceV2;
 using osu.Game.Overlays.Mods;
+using osu.Game.Rulesets.EditorModdedTestPlay.ListenerLoader.Utils;
 using osu.Game.Screens.Edit;
+using osu.Game.Screens.Edit.Components;
 using osu.Game.Screens.Edit.Components.Menus;
 
 #nullable enable
@@ -43,16 +48,73 @@ public partial class ModdedEditor(EditorLoader? loader = null) : Editor(loader)
         Mods.Disabled = false;
     }
 
+    // the old way: modified tabs
+    /* public partial class ModdedPlaybackTabControl : PlaybackControl.PlaybackTabControl
+    {
+        private static readonly double[] old_tempo_values = [0.25, 0.5, 0.75, 1];
+        private static readonly double[] new_tempo_values = [0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2];
+
+        public partial class ModdedPlaybackTabItem(double value) : PlaybackTabItem(value)
+        {
+            [BackgroundDependencyLoader]
+            private void load()
+            {
+                Width = 1f / new_tempo_values.Length;
+            }
+        }
+
+        protected override TabItem<double> CreateTabItem(double value) => new ModdedPlaybackTabItem(value);
+
+        public ModdedPlaybackTabControl(BindableNumber<double> tempoAdjustment)
+        {
+            old_tempo_values.ForEach(RemoveItem);
+            new_tempo_values.ForEach(AddItem);
+            RelativeSizeAxes = Axes.X;
+            Height = 16;
+            Current = tempoAdjustment;
+        }
+    }*/
+
+    public partial class PlaybackSpeedSliderBar : FormSliderBar<double>
+    {
+        public PlaybackSpeedSliderBar(BindableNumber<double> tempoAdjustment)
+        {
+            tempoAdjustment.MinValue = 0.1; // to avoid "TrackBass does not support Tempo specifications below 0.05"
+            tempoAdjustment.MaxValue = 3.00;
+            tempoAdjustment.Precision = 0.01;
+
+            Current = tempoAdjustment;
+            RelativeSizeAxes = Axes.X;
+            Caption = "Speed";
+            TransferValueOnCommit = false;
+            KeyboardStep = 0.1f; // 0.1x per <-/-> press
+            LabelFormat = v => $"{v:N2}x";
+        }
+    }
+
     protected override void LoadComplete()
     {
         base.LoadComplete();
 
         modSelectOverlayRegistration = registerBlockingOverlay(modSelectOverlay);
 
+        // Add menuBar Mod Select item
         var item = new EditorMenuItem("Mod Select", MenuItemType.Standard, modSelectOverlay.ToggleVisibility);
         var menuBar = this.ChildrenOfType<EditorMenuBar>().FirstOrDefault() ?? throw new NullDependencyException("EditorMenuBar not found in Editor!");
         menuBar.Add(item);
+
+        var playbackControl = this.ChildrenOfType<PlaybackControl>().FirstOrDefault() ?? throw new NullDependencyException("PlaybackControl type not found in Editor!");
+        var tempoAdjustment = (BindableNumber<double>)(playbackControl.FindInstance("tempoAdjustment") ?? throw new NullDependencyException("tempoAdjustment not found in playbackControl!"));
+        var playbackSpeedControl = (CompositeDrawable)(playbackControl.FindInstance("playbackSpeedControl") ?? throw new NullDependencyException("playbackSpeedControl not found in playbackControl!"));
+
+        // the old way: modified tabs
+        /* var oldPlaybackTabControl = playbackSpeedControl.ChildrenOfType<PlaybackControl.PlaybackTabControl>().FirstOrDefault() ?? throw new NullDependencyException("PlaybackTabControl not found in Editor!");
+        (playbackSpeedControl.FindMethod("RemoveInternal", typeof(bool)) ?? throw new NullDependencyException("Method RemoveInternal not found in PlaybackSpeedControl!"))([oldPlaybackTabControl, true]);
+        // (playbackSpeedControl.FindMethod("AddInternal", typeof(bool)) ?? throw new NullDependencyException("Method AddInternal not found in PlaybackSpeedControl!"))([new ModdedPlaybackTabControl(tempoAdjustment)]); */
+        (playbackSpeedControl.FindMethod("ClearInternal", typeof(bool)) ?? throw new NullDependencyException("Method ClearInternal not found in PlaybackSpeedControl!"))([true]); // remove all children
+        (playbackSpeedControl.FindMethod("AddInternal", typeof(bool)) ?? throw new NullDependencyException("Method AddInternal not found in PlaybackSpeedControl!"))([new PlaybackSpeedSliderBar(tempoAdjustment)]);
     }
+
 
     protected override void Dispose(bool isDisposing)
     {
